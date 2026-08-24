c80dad32bbb3cffc21192d46f89f6c7b84b562d4
OMNI <francoisbowman@gmail.com>
2026-08-24 12:03:23 -0400

    feat: route Home through locked reality-grounded redesign

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,6 @@
 import { getArticulos, getPerfumes } from "@/lib/api";
 import { CATEGORIAS_PRINCIPALES } from "@/lib/olfactiveCategories";
-import { TasteLanding } from "@/components/home/TasteLanding";
+import { AromiaHome2026 } from "@/components/home/AromiaHome2026";
 
 export const dynamic = "force-dynamic";
 
@@ -10,5 +10,5 @@ export default async function Home() {
   const categorias = CATEGORIAS_PRINCIPALES.filter((categoria) => categoria.familias.some((familia) => familias.has(familia)));
   const articulosOrdenados = [...articulos].sort((a, b) => (a.publicado_en < b.publicado_en ? 1 : -1));
 
-  return <TasteLanding perfumes={perfumes} articulos={articulosOrdenados} categorias={categorias} />;
+  return <AromiaHome2026 perfumes={perfumes} articulos={articulosOrdenados} categorias={categorias} />;
 }
